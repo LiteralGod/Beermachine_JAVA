4c1253caf9588037b538cbcaff16403badbe8c1e
Thomas <tchri20@student.sdu.dk>
2021-11-30 01:05:56 +0100

    next up is batches

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,1 +1,18 @@
-create DATABASE beermachinedb;
+DROP TABLE IF EXISTS beer_type CASCADE;
+
+create table beer_type(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+
+INSERT INTO beer_type(name)
+values ('Pilsner'),
+       ('Wheat'),
+       ('IPA'),
+       ('Stout'),
+       ('Ale'),
+       ('Alcohol Free');
+
+
+
