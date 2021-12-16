d128c5f8f87fc2b2ce013c9548ebdaa826f53cd9
Thomas <tchri20@student.sdu.dk>
2021-12-16 16:10:12 +0100

    added stateChart

--- src/main/java/Presentation/StartController.java
+++ src/main/java/Presentation/StartController.java
@@ -6,10 +6,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,7 +24,7 @@ import java.util.concurrent.Executors;
 
 public class StartController implements Initializable {
     @FXML
-    private Button startBtn, abortBtn, stopBtn, resetBtn, testBtn, saveBtn;
+    private Button startBtn, abortBtn, stopBtn, resetBtn, testBtn, saveBtn, stateChartBtn;
     @FXML
     private Label speedError, statusError;
 
@@ -64,6 +68,22 @@ public class StartController implements Initializable {
     public void setScene(ActionEvent event) throws IOException {
 
 
+    }
+
+    public void showStateChart() {
+        Parent root;
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getClassLoader().getResource("stateChart.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("State Chart");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
